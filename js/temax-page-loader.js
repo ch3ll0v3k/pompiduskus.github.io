1dc857c3357cde044af3c6fb2608cdeb2ab3e834
pompiduskus <avidon.ru@yandex.com>
2015-10-22 13:34:00 +0000

    upload

--- js/temax-page-loader.js
+++ js/temax-page-loader.js
@@ -1,13 +1,16 @@
 /* ================================================================== */
 window.addEventListener('load',function(){
-    
-    // ---------------------------------------
-    //return;
-    //__id('page_loader_bol').style.display = 'none'; 
-    //__id('page_loader_box').style.display = 'none'; 
-    //__id('center_box').style.display = 'block';
+    // -------------------------------------------------------------
+    // Page Loader
+    __id('page_loader_bol').style.display = 'none'; 
+    __id('page_loader_box').style.display = 'none'; 
+    __id('center_box').style.display = 'block';
     //document.body.style.backgroundImage = 'none';
-    // ---------------------------------------
+    // -------------------------------------------------------------
 
+
+
+
+    // -------------------------------------------------------------
 });
 /* ================================================================== */
